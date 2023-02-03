b75e1860de0347f02be81eebdc159371043fd7dd
sanazahmadifard <fda.sanaz@gmail.com>
2023-02-03 03:09:34 -0800

    Update date in function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+function Formatdate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displaytemperatur(response) {
   console.log(response.data);
   let temperaturElement = document.querySelector("#temperatur");
@@ -5,6 +30,8 @@ function displaytemperatur(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = Formatdate(response.data.dt * 1000);
   windspeedElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
