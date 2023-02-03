4e046cf14411fe23cd74ee052df3947d21094e1f
sanazahmadifard <fda.sanaz@gmail.com>
2023-02-03 09:18:24 -0800

    Added form&current button&search city function

--- src/app.js
+++ src/app.js
@@ -45,6 +45,19 @@ function displaytemperatur(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "1ee4264117b73d2263eecd562f31ef5c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displaytemperatur);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityinputElement = document.querySelector("#city-input");
+  search(cityinputElement.value);
+}
+search("London");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 let apiKey = "1ee4264117b73d2263eecd562f31ef5c";
 let city = "Moscow";
